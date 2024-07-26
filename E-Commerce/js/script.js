6376d998fda01964de0a1482a7195d2daeca1df6
RaihanSalmanBaehaqi <raihansalman24@gmail.com>
2024-07-26 18:09:08 +0700

    config project

--- E-Commerce/js/script.js
+++ E-Commerce/js/script.js
@@ -1,5 +1,6 @@
 const menuToggle = document.querySelector('.menu-toggle input');
 const navLeft = document.querySelector('.nav-left ul');
+const menuItems = document.querySelectorAll('.menu-item');
 
 menuToggle.addEventListener('click', function() {
     navLeft.classList.toggle('active');
@@ -14,3 +15,11 @@ function toggleScroll() {
         document.body.classList.remove('noscroll');
     }
 }
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLeft.classList.remove('active');
+        menuToggle.checked = false;
+        document.body.classList.remove('noscroll'); // Menambahkan baris ini untuk memastikan scroll kembali aktif
+    });
+});
